ebeaa577e9df42141b7fa123df87c20a1d188872
Garrett Madsen <garrett.l.madsen@gmail.com>
2022-12-23 04:32:17 -0500

    updates to starship and fish configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    eval (starship init fish)
+    # eval (starship init fish)
+    starship init fish | source
 end
---
+++ fish/functions/fzf_key_bindings.fish
@@ -0,0 +1,1 @@
+/home/madsen/.fzf/shell/key-bindings.fish
